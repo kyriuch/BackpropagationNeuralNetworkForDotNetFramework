cb92a48362a7aa2cc094f9bce3b3f048a5cd9dbb
kyriuch <kyriuch@gmail.com>
2019-05-10 00:04:40 +0200

    Uploading files.

---
+++ Layer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BackpropagationNeuralNetwork
+{
+	internal class Layer
+	{
+		private List<Neuron> neuronsList;
+
+		internal Layer(int neuronsCount)
+		{
+			neuronsList = new List<Neuron>(neuronsCount);
+
+			for(int i = 0; i < neuronsCount; i++)
+			{
+				neuronsList.Add(new Neuron());
+			}
+		}
+
+		internal Layer(int neuronsCount, Layer leftSideLayer)
+		{
+			neuronsList = new List<Neuron>(neuronsCount);
+
+			for (int i = 0; i < neuronsCount; i++)
+			{
+				neuronsList.Add(new Neuron(leftSideLayer.neuronsList));
+			}
+		}
+
+		internal ParallelLoopResult sumWeights()
+		{
+			return Parallel.ForEach(neuronsList, neuron => neuron.sumWeights());
+		}
+
+		internal ParallelLoopResult activate()
+		{
+			return Parallel.ForEach(neuronsList, neuron => neuron.activate());
+		}
+	}
+}
---
+++ Network.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections.Generic;
+
+namespace BackpropagationNeuralNetwork
+{
+	internal class Layers
+	{
+		private Layer inputLayer;
+		private Layer hiddenLayer;
+		private Layer outputLayer;
+
+		internal Layers(NetworkConfiguration networkConfiguration)
+		{
+			inputLayer = new Layer(networkConfiguration.InputLayerNeurons);
+			hiddenLayer = new Layer(networkConfiguration.HiddenLayerNeurons, inputLayer);
+			outputLayer = new Layer(networkConfiguration.OutputLayerNeurons, hiddenLayer);
+		}
+
+		internal Layer getInputLayer() => inputLayer;
+		internal Layer getHiddenLayer() => hiddenLayer;
+		internal Layer getOutputLayer() => outputLayer;
+	}
+
+	internal class NetworkData
+	{
+		public List<List<double>> Inputs;
+		public List<List<double>> ExpectedOutputs;
+		public Dictionary<int, List<double>> ActualOutputs;
+	}
+
+	internal struct NetworkConfiguration
+	{
+		public int InputLayerNeurons;
+		public int HiddenLayerNeurons;
+		public int OutputLayerNeurons;
+		public int MaxEras;
+		public double MinError;
+		public double LearningRate;
+		public double Momentum;
+	}
+
+    public class Network
+    {
+		private Layers layers;
+		private NetworkData networkData;
+		private NetworkConfiguration networkConfiguration;
+
+		internal Network(NetworkConfiguration networkConfiguration)
+		{
+			initializeNetwork(networkConfiguration);
+		}
+
+		private void initializeNetwork(NetworkConfiguration networkConfiguration)
+		{
+			layers = new Layers(networkConfiguration);
+			this.networkConfiguration = networkConfiguration;
+		}
+    }
+}
